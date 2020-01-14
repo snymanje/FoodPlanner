7e80935593a0912f2019474a048d8bbc081c3eb8
snymanje <jean.snyman@gmail.com>
2020-01-14 17:18:02 +0200

    init

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require("path");
+// plugin to move html file from src to dist and inject the js file(s)
+const htmlWebpackPlugin = require("html-webpack-plugin");
+var CopyWebpackPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  entry: ["./src/js/index.js"],
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "js/bundle.js"
+  },
+  devServer: {
+    contentBase: "./dist"
+  },
+  plugins: [
+    new htmlWebpackPlugin({
+      filename: "index.html",
+      template: "./src/index.html"
+    }),
+    new CopyWebpackPlugin([
+      { from: "src/css", to: "css" },
+      { from: "src/sw.js", to: "sw.js" }
+    ])
+  ],
+  // babel stuff
+  module: {
+    rules: [
+      {
+        // testing for javascript files
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  }
+};
